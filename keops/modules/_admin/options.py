fd6b5fb0ef5d8b5f6ce7ccbf038dc4bdc65d6871
Alexandre <alexandre@katrid.com>
2013-05-30 03:17:59 -0300

    .

--- keops/modules/_admin/options.py
+++
@@ -1,7 +0,0 @@
-
-#import copy
-from django.forms import models
-from django.contrib import form
-
-class ModelAdmin(form.ModelAdmin):
-    pass
--- keops/modules/_admin/sites.py
+++
@@ -1,7 +0,0 @@
-
-from django.contrib.admin import sites
-
-class AdminSite(sites.AdminSite):
-    pass
-
-site = AdminSite()
