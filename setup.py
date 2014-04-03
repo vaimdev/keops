f87a6f729d100e60185928248bae4ebca7a30651
Katrid <alexandre@katrid.com>
2014-04-03 02:35:33 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,86 @@
-#!/usr/bin/env python
+import os
+import sys
 
 from distutils.core import setup
+from distutils.sysconfig import get_python_lib
 
-setup(name='Keops',
-      version='0.4',
-      description='The Django ERP',
-      author='Alexandre L. Dias',
-      author_email='alexandre@katrid.com',
-      packages=['keops'],
-     )
-  
+# Warn if we are installing over top of an existing installation. This can
+# cause issues where files that were deleted from a more recent Django are
+# still present in site-packages. See #18115.
+overlay_warning = False
+if "install" in sys.argv:
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib/"):
+        # We have to try also with an explicit prefix of /usr/local in order to
+        # catch Debian's custom user site-packages directory.
+        lib_paths.append(get_python_lib(prefix="/usr/local"))
+    for lib_path in lib_paths:
+        existing_path = os.path.abspath(os.path.join(lib_path, "keops"))
+        if os.path.exists(existing_path):
+            # We note the need for the warning here, but present it after the
+            # command is run, so it's more likely to be seen.
+            overlay_warning = True
+            break
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+EXCLUDE_FROM_PACKAGES = []
+
+
+def is_package(package_name):
+    for pkg in EXCLUDE_FROM_PACKAGES:
+        if package_name.startswith(pkg):
+            return False
+    return True
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+keops_dir = 'keops'
+
+for dirpath, dirnames, filenames in os.walk(keops_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames and is_package(package_name):
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
+
+setup(
+    name='Keops',
+    version='0.4',
+    author='Alexandre L. Dias',
+    author_email='alexandre@katrid.com',
+    description=('Keops The Django ERP.'),
+    license='GPLv3',
+    packages=packages,
+    package_data=package_data,
+)
