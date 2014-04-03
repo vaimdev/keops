50943558f8b40b8a967ea98e60bc7b7ce4a1efc9
Katrid <alexandre@katrid.com>
2014-04-03 01:15:24 -0300

    Create setup.py
    Add setup.py file (setup script)

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Keops',
+      version='0.4',
+      description='The Django ERP',
+      author='Alexandre L. Dias',
+      author_email='alexandre@katrid.com',
+     )
+  
