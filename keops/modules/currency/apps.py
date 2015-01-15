dacc38eb78dd6b05b021b75a7f262876a801d351
katrid <alexandre@katrid.com>
2015-01-15 19:06:39 -0200

    Redesign structure

--- keops/modules/currency/apps.py
+++ keops/modules/currency/apps.py
@@ -1,9 +1,10 @@
 from django.utils.translation import ugettext_lazy as _
-from keops.apps import Module
+from keops.apps import AppConfig
 
 
-class Currency(Module):
+class CurrencyConfig(AppConfig):
     version = '0.1'
     verbose_name = 'Currency'
     description = _('Currency integration module')
     category = _('Business')
+    requirements = ['keops.modules.internationalization']
