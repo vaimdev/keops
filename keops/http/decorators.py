29624df47caa4d8057a1c6909a41507e315110a4
katrid <alexandre@katrid.com>
2015-02-15 18:38:21 -0200

    Django 1.7 already comes with json decorator

--- keops/http/decorators.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-
-# json decorator
-def json_response(func):
-    def decorator(request, *args, **kwargs):
-        objects = func(request, *args, **kwargs)
-        return JsonResponse(objects)
-    return decorator
